2e7068a40ba3df1e1b9636a0f4763f3cbb79c2ad
pinetree <eodnjs568@gmail.com>
2023-08-04 16:45:28 +0900

    feat : Reply Entity, DTO, Service, Repository 계층 작성 및 간단한 테스트 코드 작성

--- part3/board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ part3/board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -3,11 +3,19 @@ package org.zerock.board.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
+    /**
+     * Board 삭제 시에 댓글들 삭제
+     */
     @Modifying
     @Query("delete from Reply r where r.board.bno = :bno")
     void deleteByBno(Long bno);
+
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
--- part3/board/src/test/java/org/zerock/board/repository/ReplyRepositoryTest.java
+++ part3/board/src/test/java/org/zerock/board/repository/ReplyRepositoryTest.java
@@ -1,11 +1,13 @@
 package org.zerock.board.repository;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -40,5 +42,23 @@ class ReplyRepositoryTest {
         System.out.println("reply = " + reply);
         System.out.println("reply.getBoard() = " + reply.getBoard());
     }
+
+    @Test
+    @DisplayName("특정 게시글의 번호를 토대로 게시글 리스트를 조회하는 테스트")
+    public void testListByBoard() {
+        List<Reply> replyList = replyRepository.getRepliesByBoardOrderByRno(
+                Board.builder()
+                        .bno(97L)
+                        .build()
+        );
+        replyList.forEach(
+                reply-> {
+                    System.out.println(reply);
+                }
+        );
+
+    }
+
+
 
 }
